6b1e17bb37be9504b1057d908794fbc7947fd4bc
OlenaB <elena.yefimchuk@gmail.com>
2025-03-13 00:42:28 -0600

    fixes for server post section work_togather

--- src/js/work_together.js
+++ src/js/work_together.js
@@ -4,7 +4,8 @@ import * as basicLightbox from 'basiclightbox';
 import 'basicLightbox/dist/basicLightbox.min.css';
 import axios from 'axios';
 
-const modalFooter = basicLightbox.create(`
+const modalFooter = basicLightbox.create(
+  `
     <div class="modal">
     <button class="modal-close">
      <svg class="modal-close-icon">
@@ -15,46 +16,49 @@ const modalFooter = basicLightbox.create(`
     <p class="modal-text">The manager will contact you shortly to discuss further details and opportunities for cooperation. Please stay in touch.</p>
     </div>
 	
-`, {
-    closable: true
-})
-
-
+`,
+  {
+    closable: true,
+  }
+);
 
 const refs = {
-    emailInp: document.querySelector("#email"),
-    commentInp: document.querySelector("#comment"),
-    footerForm: document.querySelector(".footer-form"),
-}
-refs.footerForm.addEventListener("submit", onFormSubmit);
-
-
+  emailInp: document.querySelector('#email'),
+  commentInp: document.querySelector('#comment'),
+  footerForm: document.querySelector('.footer-form'),
+};
+refs.footerForm.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
-    event.preventDefault();
-    const email = event.target.elements.email.value.trim();
-    const comment = event.target.elements.comment.value.trim();
-    const data = {
-        email: email,
-        comment: comment,
-    }
-    if (!email || !comment) {
-        iziToast.error({
-            title: 'Error',
-            message: `All form fields must be filled in`,
-            position: 'topCenter',
-            closeOnClick: true,
-        });
-    }
-    else {
-        console.log(data);
-        axios.post('https://portfolio-js.b.goit.study/api/requests', data);
+  event.preventDefault();
+  const email = event.target.elements.email.value.trim();
+  const comment = event.target.elements.comment.value.trim();
+  const data = {
+    email: email,
+    comment: comment,
+  };
+  if (!email || !comment) {
+    iziToast.error({
+      title: 'Error',
+      message: `All form fields must be filled in`,
+      position: 'topCenter',
+      closeOnClick: true,
+    });
+    return;
+  } else {
+    console.log(data);
+    axios
+      .post('https://portfolio-js.b.goit.study/api/requests', data)
+      .then(response => {
+        console.log('data sended');
         modalFooter.show();
-        document.querySelector(".modal-close").addEventListener("click", (e) => {
-            modalFooter.close();
-        })
+        document.querySelector('.modal-close').addEventListener('click', e => {
+          modalFooter.close();
+        });
         refs.footerForm.reset();
-    };
-
+      })
+      .catch(error => {
+        console.error('data send fail');
+      });
+  }
 }
-
